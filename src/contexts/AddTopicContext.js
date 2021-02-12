4908e6910a66aca0796cbd71600ea94c61eb5b71
nurbek <selecid_beka@icloud.com>
2021-02-12 15:53:07 +0600

    Добавили API адреса для db.json

--- src/contexts/AddTopicContext.js
+++ src/contexts/AddTopicContext.js
@@ -1,30 +1,36 @@
 import React, { useReducer } from 'react';
 import axios from 'axios';
 
+const API_TOPICS = "http://localhost:8000/topics";
+const API_REGISTRATION = "http://localhost:8000/accounts";
+
 export const addTopicContext = React.createContext();
 
 const INIT_STATE = {
     topics: []
-}
+};
 
 const reducer = (state = INIT_STATE, action) => {
     switch (action.type) {
-       
+
         default: return state
     }
-}
+};
 
 const AddTopicContextProvider = ({ children }) => {
     const [state, dispatch] = useReducer (reducer, INIT_STATE)
-    
+
     // const addContact = async (newContact) => {
     //     await axios.post('http://localhost:8000/contactData', newContact)
-        
+
     //     getContactData()
     // }
 
     return (
-        <addTopicContext.Provider>
+        <addTopicContext.Provider value={{
+            API_REGISTRATION,
+            API_TOPICS
+        }} >
             {children}
         </addTopicContext.Provider>
     )
